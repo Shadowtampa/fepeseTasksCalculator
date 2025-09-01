d1b23b2e1fa31bb517c2266ed4b2e9653cc9648d
Luis Gomes <luisgomesbcc@gmail.com>
2025-09-01 18:00:10 -0300

    corrigindo problema de build

--- src/utils/calculations.ts
+++ src/utils/calculations.ts
@@ -1,7 +1,9 @@
-export const calculateTotalHours = (tasks) => {
-    return tasks.reduce((total, task) => total + task.horas, 0);
+import type { Task } from "../types";
+
+export const calculateTotalHours = (tasks : Task[]) => {
+    return tasks.reduce((total, task) => total + task.hours, 0);
 };
 
-export const convertHoursToDays = (hours) => {
+export const convertHoursToDays = (hours : number) => {
     return hours / 24;
 };
