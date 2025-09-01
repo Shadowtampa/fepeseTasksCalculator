68e9f3651c1538ec796ac8c21a944cfa79ba3cdb
Luis Gomes <luisgomesbcc@gmail.com>
2025-09-01 18:19:21 -0300

    deixando o nome mais aceitável

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <header >
-            <h1>Tasks LABTRANS</h1>
+            <h1>Calculadora de horas PATO🦆</h1>
+            <h3>A solução pros problemas da PO</h3>
         </header>
     );
 };
